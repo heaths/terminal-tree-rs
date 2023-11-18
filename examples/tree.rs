1462d2223ccd9fda5725cf356833fad63889700c
Heath Stewart <heaths@microsoft.com>
2023-11-17 16:07:29 -0800

    Add optional support for clap

--- examples/tree.rs
+++ examples/tree.rs
@@ -1,12 +1,35 @@
 // Copyright 2023 Heath Stewart.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
-use terminal_tree::TreeBuilder;
+#![allow(dead_code, unused_imports, unused_variables)]
+
+use clap::Parser;
+use crossterm::style;
+use terminal_tree::{
+    clap::{ColorValue, ColorValueParser},
+    TreeBuilder,
+};
 
 fn main() {
-    let builder = TreeBuilder::default();
+    let args = Args::parse();
+    let builder = TreeBuilder::default()
+        .colors(args.colors)
+        .indentation(args.indent);
+
     for i in 0..8 {
         builder.indent(i);
         println!();
     }
+}
+
+#[derive(Parser)]
+#[command(about = "Shows a simple tree of branches and leaves.")]
+struct Args {
+    /// Optional array of colors to use repeatedly for branch lines.
+    #[arg(long, value_delimiter = ',', default_values_t = ColorValueParser::default_values(TreeBuilder::COLORS))]
+    colors: Vec<ColorValue>,
+
+    /// Optional level of indentation.
+    #[arg(long, default_value_t = TreeBuilder::INDENTATION)]
+    indent: u8,
 }
